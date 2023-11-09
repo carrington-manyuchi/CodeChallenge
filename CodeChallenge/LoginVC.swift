806e2c2f99e8bbc53d35b5d305c9f351958652fa
Carrington <Manyuchi>
2023-11-09 20:35:24 +0200

    Activated  NSLayout Constraints

--- CodeChallenge/LoginVC.swift
+++ CodeChallenge/LoginVC.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class LoginVC: UIViewController {
     
-    
     private let loginTitleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -17,36 +16,61 @@ class LoginVC: UIViewController {
         label.text = "Login to your account"
         label.textColor = .label
         return label
-
     }()
     
     private let emailTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.keyboardType = .emailAddress
-        textField.borderStyle = .line
+        textField.borderStyle = .roundedRect
         textField.placeholder = "Enter email"
         return  textField
     }()
-    
     
     private let passwordtextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.isSecureTextEntry = true
         textField.placeholder = "Enter password"
+        textField.borderStyle = .roundedRect
         return textField
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
+        view.backgroundColor = .systemBackground
         view.addSubview(loginTitleLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordtextField)
+        configureConstraints()
     }
     
-    
-    
+    private func configureConstraints() {
+        
+        let loginTitleLabelConstraints = [
+            loginTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            loginTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ]
+        
+        let emailTextFieldConstraints = [
+            emailTextField.topAnchor.constraint(equalTo: loginTitleLabel.bottomAnchor, constant: 30),
+            emailTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            emailTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        let passwordtextFieldConstraints = [
+            passwordtextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 40),
+            passwordtextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            passwordtextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30),
+            passwordtextField.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        
+        NSLayoutConstraint.activate(loginTitleLabelConstraints)
+        NSLayoutConstraint.activate(emailTextFieldConstraints)
+        NSLayoutConstraint.activate(passwordtextFieldConstraints)
+    }
 
 
 
