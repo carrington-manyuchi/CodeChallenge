256216f164ec0f7cd0fd3d098800a2a376eec3da
Carrington <Manyuchi>
2023-11-09 19:22:55 +0200

    added constraints

--- CodeChallenge/OnboardingVC.swift
+++ CodeChallenge/OnboardingVC.swift
@@ -14,12 +14,11 @@ class OnboardingVC: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         label.text = "See what's happening in the world of IOS right now."
-        label.font = .systemFont(ofSize: 35, weight: .heavy)
+        label.font = .systemFont(ofSize: 32, weight: .heavy)
         label.textAlignment = .center
         label.textColor = .label
         return label
     }()
-    
     
     private let nextButton: UIButton = {
         let button = UIButton(type: .system)
@@ -33,13 +32,30 @@ class OnboardingVC: UIViewController {
         return button
     }()
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
-
+        view.addSubview(welcomeLabel)
+        view.addSubview(nextButton)
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        let welcomeLabeConstraints = [
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+                
+        let nextButtonConstraints = [
+            nextButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 20),
+            nextButton.widthAnchor.constraint(equalToConstant: 250),
+            nextButton.heightAnchor.constraint(equalToConstant: 65),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(welcomeLabeConstraints)
+        NSLayoutConstraint.activate(nextButtonConstraints)
     }
 
 }
