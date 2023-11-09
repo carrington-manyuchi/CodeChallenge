c8ba7c5cbdc5179751ca78cc59a3bb6aeca6f9f4
Carrington <Manyuchi>
2023-11-09 20:12:52 +0200

    Login Title Label

--- CodeChallenge/LoginVC.swift
+++ CodeChallenge/LoginVC.swift
@@ -12,14 +12,26 @@ class LoginVC: UIViewController {
     
     private let loginTitleLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.text = "Login to your account"
+        label.textColor = .label
         return label
-        
+
     }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        view.addSubview(loginTitleLabel)
     }
+    
+    
+    
+
 
 
 }
