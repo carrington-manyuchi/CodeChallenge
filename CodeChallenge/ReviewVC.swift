b327f1627a1fa12fab65c2e1d164ed1eefe82da5
Carrington <Manyuchi>
2023-11-10 11:29:19 +0200

    personal details labels on review

--- CodeChallenge/ReviewVC.swift
+++ CodeChallenge/ReviewVC.swift
@@ -33,7 +33,7 @@ class ReviewVC: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Carrington Manyuchi"
-        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.font = .systemFont(ofSize: 21, weight: .medium)
         return label
     }()
     
@@ -41,34 +41,68 @@ class ReviewVC: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "carrington@gmail.com"
-        label.font = .systemFont(ofSize: 15, weight: .regular)
+        label.font = .systemFont(ofSize: 16, weight: .regular)
         return label
     }()
     
     private let dateOfBirthLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Carrington Manyuchi"
-        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.text = "20 Dec 1998"
+        label.font = .systemFont(ofSize: 16, weight: .regular)
         return label
     }()
     
     private let genderLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Carrington Manyuchi"
-        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.text = "Male"
+        label.font = .systemFont(ofSize: 16, weight: .regular)
         return label
     }()
     
-
+    private let additionalInformationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Additional Information"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    private let colorNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "20 Dec 1998"
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private let placeOfBirthLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "20 Dec 1998"
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private let residentialAddressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "20 Dec 1998"
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        configureConstraints()
     }
     
     private func setupUI() {
         view.backgroundColor = .systemBackground
+        
+        //personal details
         view.addSubview(personalDetailsLabel)
         view.addSubview(avatarImageView)
         view.addSubview(displayFullNameLabel)
@@ -77,5 +111,44 @@ class ReviewVC: UIViewController {
         view.addSubview(genderLabel)
     }
     
-    private func
+    private func configureConstraints() {
+        let personalDetailsLabelConstraints = [
+            personalDetailsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            personalDetailsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+        ]
+        
+        let avatarImageViewConstraints = [
+            avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            avatarImageView.topAnchor.constraint(equalTo: personalDetailsLabel.bottomAnchor, constant: 30),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 100),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 100),
+        ]
+        
+        let displayFullNameLabelConstraints = [
+            displayFullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 15),
+            displayFullNameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor)
+        ]
+        
+        let emailLabelConstraints = [
+            emailLabel.leadingAnchor.constraint(equalTo: displayFullNameLabel.leadingAnchor),
+            emailLabel.topAnchor.constraint(equalTo: displayFullNameLabel.bottomAnchor, constant: 12),
+        ]
+        
+        let dateOfBirthLabelConstraints = [
+            dateOfBirthLabel.leadingAnchor.constraint(equalTo: displayFullNameLabel.leadingAnchor),
+            dateOfBirthLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 12),
+        ]
+        
+        let genderLabelConstraints = [
+            genderLabel.leadingAnchor.constraint(equalTo: displayFullNameLabel.leadingAnchor),
+            genderLabel.topAnchor.constraint(equalTo: dateOfBirthLabel.bottomAnchor, constant: 12)
+        ]
+        
+        NSLayoutConstraint.activate(personalDetailsLabelConstraints)
+        NSLayoutConstraint.activate(avatarImageViewConstraints)
+        NSLayoutConstraint.activate(displayFullNameLabelConstraints)
+        NSLayoutConstraint.activate(emailLabelConstraints)
+        NSLayoutConstraint.activate(dateOfBirthLabelConstraints)
+        NSLayoutConstraint.activate(genderLabelConstraints)
+    }
 }
