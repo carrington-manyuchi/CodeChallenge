1ee570000dca4d78f94769fc87ffdff2344c0e4d
Carrington <Manyuchi>
2023-11-11 09:41:16 +0200

    Success View Controller improvements on UI

--- CodeChallenge/Controllers/SuccessViewController.swift
+++ CodeChallenge/Controllers/SuccessViewController.swift
@@ -21,7 +21,7 @@ class SuccessViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Success"
-        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        label.font = .systemFont(ofSize: 21, weight: .semibold)
         label.textColor = .label
         return label
     }()
@@ -30,15 +30,72 @@ class SuccessViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse est leo, vehicula eu eleifend non, auctor ut arcu, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse est leo, vehicula eu eleifend non, auctor ut arcu"
-        label.font = .systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 15)
         label.sizeToFit()
-        label.textAlignment = .center
+        label.textAlignment = .justified
         return label
+    }()
+    
+    private let successDoneButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Submit", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 21, weight: .bold)
+        button.backgroundColor = .blue
+        button.tintColor = .white
+        button.layer.cornerRadius = 8
+        button.layer.shadowColor = UIColor.systemGray.cgColor
+        button.layer.shadowOffset = CGSize(width: 3, height: 7)
+        button.layer.shadowOpacity = 0.8
+        button.layer.shadowRadius = 3
+        return button
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupViews()
+        configureConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(successImageView)
+        view.addSubview(successLabel)
+        view.addSubview(successMessageLabel)
+        view.addSubview(successDoneButton)
+    }
+    
+    private func configureConstraints() {
+        let successImageViewConstraints = [
+            successImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            successImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            successImageView.heightAnchor.constraint(equalToConstant: 120),
+            successImageView.widthAnchor.constraint(equalToConstant: 120),
+        ]
+        
+        let successLabelConstraints = [
+            successLabel.topAnchor.constraint(equalTo: successImageView.bottomAnchor, constant: 50),
+            successLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ]
+        
+        let successMessageLabelConstraints = [
+            successMessageLabel.topAnchor.constraint(equalTo: successLabel.bottomAnchor, constant: 20),
+            successMessageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            successMessageLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30),
+        ]
+        
+        let successDoneButtonConstraint = [
+            successDoneButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            successDoneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            successDoneButton.widthAnchor.constraint(equalToConstant: 200),
+            successDoneButton.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(successImageViewConstraints)
+        NSLayoutConstraint.activate(successLabelConstraints)
+        NSLayoutConstraint.activate(successMessageLabelConstraints)
+        NSLayoutConstraint.activate(successDoneButtonConstraint)
     }
     
 }
