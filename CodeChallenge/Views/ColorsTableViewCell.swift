b25d0b24f39478126e30d94b5e3a8ef205009456
Carrington <Manyuchi>
2023-11-11 09:46:44 +0200

    Colors View Controller and Table View Cell

---
+++ CodeChallenge/Views/ColorsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ColorsTableViewCell.swift
+//  CodeChallenge
+//
+//  Created by DA MAC M1 157 on 2023/11/11.
+//
+
+import UIKit
+
+class ColorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
